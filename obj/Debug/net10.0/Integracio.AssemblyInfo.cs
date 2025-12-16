31317559d1a8a0fda311cd5180f2feff0a176efd
Unai Almiñana <unai.alminana.7e9@itb.cat>
2025-12-16 12:38:59 +0100

    fix: added the project of xunit 2

--- obj/Debug/net10.0/Integracio.AssemblyInfo.cs
+++ obj/Debug/net10.0/Integracio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Integracio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c07f8688b417a889e20dde7922bb14488ddca5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb9265e2302e360d18cb383ca89280c334935b8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Integracio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Integracio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ xUnitLibrary/xUtils.cs
@@ -0,0 +1,49 @@
+﻿namespace xUnitLibrary
+{
+    public class xUtils
+    {
+        public static int CalculateResult(int a, int b, int c)
+        {
+
+            if (a > 0)
+            {
+                if (b > 0)
+                {
+                    if (c > 0)
+                    {
+                        return a + b + c;
+                    }
+                    else
+                    {
+                        return a + b;
+
+                    }
+                }
+                else
+                {
+                    return a;
+
+                }
+            }
+            else
+            {
+                return 0;
+
+            }
+        }
+
+        public static string SendMsg(int result)
+        {
+            //Optimitzem la complexitat ciclomatica
+            switch (result)
+            {
+                case 0:
+                    return "El resultat és zero.";
+                case < 0:
+                    return "El resultat és negatiu.";
+                default:
+                    return "El resultat és positiu.";
+            }
+        }
+    }
+}
